9348c79895af41a4fbd03a540670b18f32fa02f8
Hawxsw <williamsilva1942@gmail.com>
2025-02-03 07:33:24 -0300

    feat: Energy Compensation Simulation Project completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadsModule } from './leads/leads.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [LeadsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
